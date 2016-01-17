724736a87db50ea5a0c5f438fa182e5822c793df
joanna-chen <joannachen.x@gmail.com>
2016-01-17 03:55:41 -0500

    welcome panel

---
+++ LM/Assets/Scripts/closebutton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class closebutton : MonoBehaviour {
+
+	public GameObject menu;
+	public GameObject text;
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			menu.SetActive (false);
+			text.SetActive (false);
+		}
+	}
+
+}
